5d5dc0cb2f0308c8f759393146d4072bec6c7efc
Ashraful Islam Sohag <sohagali09876543@gmail.com>
2024-10-22 20:21:01 +0600

    userRegister

--- app/Helper/JWTToken.php
+++ app/Helper/JWTToken.php
@@ -1,14 +1,22 @@
 <?php
 
 namespace App\Helper;
+use Firebase\JWT\JWT;
 class  JWTToken
 {
   function createToken(){
 
   }
 
-  function verifyToken(){
+  function verifyToken($userEmail){
      $key=env('JWT_KEY');
+     $payload = [
+        'iss' => 'laravel-token',
+        'iat' => time(),
+        'exp' => time() + (60 * 60),
+        'userEmail' => $userEmail
+    ];
+    JWT::encode($payload, $key);
   }
 
 
